3c6c712d89b18959d641551e3219e94c371927d1
NatalyaZinger <nata.zinger@gmail.com>
2022-11-04 15:35:19 +0500

    test

---
+++ src/test/java/CardOrderTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class CardOrderTest {
+    @Test
+    void shouldRegister(){
+        open("http://localhost:9999");
+        $("[placeholder='Город']").setValue("Челябинск");
+
+
+    }
+
+
+
+}
